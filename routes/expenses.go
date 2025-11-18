4332e2146de36a3cf56a76eff3806bcdc39f0e76
sui-tyan <mallari23adrian@gmail.com>
2025-11-18 22:31:38 +0800

    feat: added dummy recent expenses response
    added dummy data to be returned from /expenses/recent

--- routes/expenses.go
+++ routes/expenses.go
@@ -9,7 +9,21 @@ func AddExpenseRoutes(router *gin.RouterGroup) {
 	})
 
 	router.GET("/recent", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "List of expenses"})
+
+		type ExpenseData struct {
+			Label  string  `json:"label"`
+			Amount float64 `json:"amount"`
+		}
+
+		recent_expenses := []ExpenseData{
+			{Label: "Food", Amount: 150.75},
+			{Label: "Travel", Amount: 80.50},
+			{Label: "Guitar", Amount: 60.00},
+			{Label: "Transaction Fees", Amount: 45.25},
+			{Label: "Other", Amount: 120.00},
+		}
+
+		c.JSON(200, gin.H{"data": recent_expenses})
 	})
 
 }
